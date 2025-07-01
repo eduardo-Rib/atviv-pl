1304afb4a1f3106818faad74cdd7c04ea15ef9e0
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2025-06-30 23:22:51 -0300

    corrigindo rotas

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:32831',
+  baseURL: 'http://localhost:32831/cliente',
 });
 
-// export const apiClient = {
-//   criarCliente: (data: any) => api.post('/cliente', data),
-//   listarClientes: (search?: string) => api.get('/cliente', { params: { search } }),
-//   buscarCliente: (id: number) => api.get(`/cliente/${id}`),
-//   atualizarCliente: (id: number, data: any) => api.put(`/cliente/${id}`, data),
-//   deletarCliente: (id: number) => api.delete(`/cliente/${id}`)
-// };
-
 export const apiClient = {
-  criarCliente: (data: any) => api.post('/cliente/cadastrar', data),
-  listarClientes: (search?: string) => api.get('/cliente/clientes'),
-  buscarCliente: (id: number) => api.get(`/cliente/${id}`),
-  atualizarCliente: (data: any) => api.put('/cliente/atualizar', data),
-  deletarCliente: (id: number) => api.delete('/cliente/excluir', { data: { id } })
+  listarClientes: () => api.get('/clientes'),
+  buscarCliente: (id: number) => api.get(`/${id}`),
+  criarCliente: (data: any) => api.post('/cadastrar', data),
+  atualizarCliente: (data: any) => api.put('/atualizar', data),
+  deletarCliente: (id: number) => api.delete('/excluir', { data: { id } })
 };
